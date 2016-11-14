2d1ff39395c691ec3246249fe0d723a6dba12646
Dwyane Shi <dwyane.shi@gmail.com>
2016-11-14 14:23:49 -0500

    Delete some test scripts

--- testsuite/test-submit-default.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source test-config.sh
-
-SubmitDefaultStandardTests() {
-    BasicJobSubmit hamster.${submissiontype}-hadoop-default-run-hadoopterasort
-    BasicJobSubmit hamster.${submissiontype}-hadoop-default-run-hadoopterasort-no-local-dir
-}
